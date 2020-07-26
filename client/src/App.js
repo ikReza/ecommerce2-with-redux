2411f0ea31ea0bbcdb6f40b5fee0f81793a241c4
ikReza <ireza.kaiser00@gmail.com>
2020-07-26 14:39:01 +0600

    register screen

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SignInScreen from "./components/screens/SignInScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
 
 function App() {
   return (
@@ -13,9 +14,9 @@ function App() {
         {/* <Route path="/" exact component={Home} />
         <Route path="/products" component={ProductsScreen} /> */}
         <Route path="/signin" exact component={SignInScreen} />
+        <Route path="/register" component={RegisterScreen} />
         {/* <Route path="/product/:id" exact component={ProductScreen} />
-        <Route path="/cart/:id?" component={CartScreen} />
-        <Route path="/register" component={Register} /> */}
+        <Route path="/cart/:id?" component={CartScreen} /> */}
       </Switch>
       <Footer />
     </Router>
---
+++ client/src/components/screens/RegisterScreen.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Grid, Box, TextField, Button, Typography } from "@material-ui/core";
+// import { useDispatch, useSelector } from "react-redux";
+// import { register } from "../actions/userActions";
+
+const RegisterScreen = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  //   const userRegister = useSelector((state) => state.userRegister);
+  //   const { loading, userInfo, error } = userRegister;
+  //   const dispatch = useDispatch();
+  const userInfo = false;
+  const error = false;
+  const loading = false;
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push("/");
+    }
+    return () => {};
+  }, [userInfo]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== rePassword) {
+      alert("Password don't match");
+      setRePassword("");
+    } else {
+      //dispatch(register(name, email, password));
+    }
+  };
+
+  return (
+    <Grid
+      container
+      justify="center"
+      style={{ height: "70vh", alignItems: "center" }}
+    >
+      <Grid item xs={10} sm={7} md={5}>
+        <Box component="form" onSubmit={handleSubmit}>
+          <Typography align="center" variant="h4" gutterBottom>
+            Create Account
+          </Typography>
+          {loading && (
+            <Typography variant="subtitle2">Loading. .. ...</Typography>
+          )}
+          {error && <Typography variant="subtitle2">{error}</Typography>}
+          <TextField
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            label="Name"
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            label="Email"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            label="Password"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            label="Re-Enter Password"
+            type="password"
+            required
+            value={rePassword}
+            onChange={(e) => setRePassword(e.target.value)}
+          />
+          <Button
+            variant="outlined"
+            fullWidth
+            type="submit"
+            className="signin-btn-main"
+          >
+            Register
+          </Button>
+          <Typography align="center">
+            Already a member?{" "}
+            <Button
+              component={Link}
+              to="/signin"
+              className="signup-btn-secondary"
+            >
+              Sign In
+            </Button>{" "}
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default RegisterScreen;
